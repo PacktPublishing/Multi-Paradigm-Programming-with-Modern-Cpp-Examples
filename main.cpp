39e3096c3f5064cbb84851fc4229ec30bd8af374
Georgy Pashkov <gosha.pashkov@gmail.com>
2020-01-25 13:43:56 +0100

    After 6.1

--- main.cpp
+++ main.cpp
@@ -4,24 +4,28 @@ namespace constexpr_math {
     constexpr auto pi = 3.14159265358979323846264;
     constexpr auto epsilon = 0.0001;
 
-    constexpr double abs(double value){
+    template<typename number_t>
+    constexpr double abs(number_t value){
         return value > 0 ? value : -value;
     }
 
-    constexpr bool equal(double a, double b){
+    template<typename number_t>
+    constexpr bool equal(number_t a, double b){
         return abs(a - b) < epsilon;
     }
 
-    constexpr double rad(double deg){
-        return deg * pi / 180.0;
+    template<typename number_t>
+    constexpr number_t rad(number_t deg){
+        return deg * number_t{pi} / number_t{180.0};
     }
 
-    static_assert(equal(rad(90), pi / 2));
+    static_assert(equal(rad(90.0), pi / 2));
 
-    constexpr double cos(double rad){
+    template<typename number_t>
+    constexpr double cos(number_t rad){
         const auto n = 20; // higher n = better precision
-        long double sum = 1;
-        long double t = 1;
+        number_t sum = 1;
+        number_t t = 1;
 
         // Approximate using Taylor series
         for (auto i = 1; i <= n; ++i){
@@ -33,41 +37,42 @@ namespace constexpr_math {
     }
 
     static_assert(equal(cos(0), 1));
-    static_assert(equal(cos(rad(60)), 0.50));
+    static_assert(equal(cos(rad(60.0)), 0.50));
 
-    template<unsigned short precision_v>
+    template<unsigned short precision_v, typename number_t>
     class fast_cos{
         public:
         static constexpr auto precision = precision_v;
         static constexpr auto nof_values = precision * 360;
 
-        double cos_table[nof_values];
+        number_t cos_table[nof_values];
 
         constexpr fast_cos() : cos_table() {
             for (auto i = size_t{0}; i < nof_values; ++i){
-                cos_table[i] = constexpr_math::cos(rad(static_cast<double>(i) / precision));
+                cos_table[i] = constexpr_math::cos(rad(static_cast<number_t>(i) / precision));
             }
         }
 
-        constexpr double value_at(unsigned table_index) const {
+        constexpr number_t value_at(unsigned table_index) const {
             if (table_index / precision > 360)
                 throw std::runtime_error ("Angle must be under 360 degrees");
             
             return cos_table[table_index];
         }
 
-        constexpr double cos(double deg) const {
+        constexpr double cos(number_t deg) const {
             // todo: error checking!
             return value_at(static_cast<unsigned>(deg * precision));
         }
-
     };
 
-    static_assert(equal(fast_cos<10>{}.cos(60), 0.5));
+    static_assert(equal(fast_cos<10, double>{}.cos(60), 0.5));
 }
 
 int main(int, char**) {
     constexpr int precision = 10;
-    auto c = constexpr_math::fast_cos<precision>{};
+    constexpr auto c = constexpr_math::fast_cos<precision, double>{};
+    constexpr auto c2 = constexpr_math::fast_cos<precision, float>{};
+    constexpr auto c3 = constexpr_math::fast_cos<precision, int>{};
     std::cout << c.cos(60.1) << std::endl;
 }
