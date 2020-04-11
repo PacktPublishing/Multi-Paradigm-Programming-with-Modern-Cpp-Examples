143d65a66c6456dcb73fcb0532fe97a95819d6d7
Georgy Pashkov <gosha.pashkov@gmail.com>
2020-04-11 19:10:16 +0200

    After section 8

--- fp_math.h
+++ fp_math.h
@@ -1,14 +1,9 @@
 #pragma once
 
 #include "constexpr_math.h"
-#include <type_traits>
 
 namespace fp_math {
-
-    template<typename T>
-    concept Integral = std::is_integral<T>::value;
-
-    template<unsigned short precision_v>
+    template<unsigned short precision_v = 4>
     class number {
         public:
         using storage_type = long long;
@@ -17,15 +12,13 @@ namespace fp_math {
         constexpr static auto precision = precision_v;
         constexpr static auto offset = constexpr_math::pow(storage_type{10}, precision_v);
 
-        constexpr number() = default;
+
 
         template<typename fp_type>
         constexpr number(const fp_type &value) noexcept{
-            auto dec_part = static_cast<storage_type>(value);
-            value_ = dec_part * offset;
-
-            auto frac_part = value - dec_part;
-            value_ += frac_part * offset;
+            value_ = static_cast<storage_type>(value) * offset;
+            value_ += static_cast<storage_type>(
+                (value - static_cast<storage_type>(value)) * offset);
         }
 
         constexpr storage_type int_part() const noexcept{
@@ -36,14 +29,10 @@ namespace fp_math {
             return value_ % (int_part() * offset);
         }
 
+        constexpr number() = default;
+
         constexpr auto operator==(const same_precision_number &other) const noexcept{
             return value_ == other.value_;
-        }
-
-        constexpr auto operator -() const noexcept{
-            same_precision_number n;
-            n.value_ = -value_;
-            return n;
         }
 
         constexpr auto operator <(const same_precision_number &rhs) const noexcept {
@@ -53,6 +42,14 @@ namespace fp_math {
         constexpr auto operator >(const same_precision_number &rhs) const noexcept {
             return value_ > rhs.value_;
         } 
+        
+        
+        constexpr auto operator -() const noexcept{
+            same_precision_number n;
+            n.value_ = -value_;
+            return n;
+        }
+
 
         constexpr auto operator +(const same_precision_number &rhs) const noexcept{
             same_precision_number n;
@@ -66,71 +63,22 @@ namespace fp_math {
             return n;
         }
 
-        constexpr auto operator += (const same_precision_number &rhs) noexcept {
-            value_ += rhs.value_;
-            return *this;
-        }
 
-        constexpr auto operator -= (const same_precision_number &rhs) noexcept {
-            value_ -= rhs.value_;
-            return *this;
-        }
-
-        //TODO: implement the operators
-        #pragma clang diagnostic push
-        #pragma clang diagnostic ignored "-Wexceptions"
-        constexpr auto operator * (const same_precision_number &rhs) const noexcept{
-            throw std::logic_error("Not implemented");
-            return *this; // for return type deduction
-        }
-
-        constexpr auto operator / (const same_precision_number &rhs) const noexcept{
-            throw std::logic_error("Not implemented");
-            return *this;
-        }
-
-        constexpr auto operator *= (const same_precision_number &rhs) noexcept {
-            throw std::logic_error("Not implemented");
-            return *this;
-        }
-        constexpr auto operator /= (const same_precision_number &rhs) noexcept {
-            throw std::logic_error("Not implemented");
-            return *this;
-        }
-
-        #pragma clang diagnostic pop
 
         private:
         storage_type value_ = 0;
     };
 }
 
-namespace constexpr_math {
-    template<unsigned short precision_v>
-    struct enable_exact_compare<fp_math::number<precision_v>> {
-        constexpr static auto value = true;
-    };
-}
-
-namespace fp_math {
+namespace fp_math::test {
     using n4 = number<4>;
-
-    //static_assert(constexpr_math::cos(n4{0}) == 1);
-
     static_assert(n4::offset == 10000);
     static_assert(n4{-33.42}.int_part() == -33);
     static_assert(n4{-33.42}.frac_part() == -4200);
 
-    static_assert(n4{42}.int_part() == 42);
-    static_assert(n4{42}.frac_part() == 0);
-
-    static_assert(n4{42} == n4{42});
-    static_assert(n4{42.1} < n4{42.2});
-
     static_assert(constexpr_math::abs(n4{-42}) == n4{42});
-    static_assert(constexpr_math::equal(n4{42}, n4{42}));
 
-    //static_assert(constexpr_math::fast_cos<10, n4>{}.cos(60) == n4{0.5});
-
-    static_assert(!constexpr_math::equal(number<9>{1.0}, number<9>{1.0000001}));
+    using n9 = number<9>;
+    static_assert(constexpr_math::equal(n9{42}, n9{42.0000001}));
+    static_assert(n9{42} == n9{42.0000001});
 }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,40 @@
-#include "constexpr_math.h"
-#include "fp_math.h"
+#include <chrono>
 #include <iostream>
+#include <numeric>
+#include <thread>
+#include <vector>
 
-int main(int, char**) {
-    constexpr int precision = 10;
-    constexpr auto c = constexpr_math::fast_cos<precision, double>{};
-    constexpr auto c2 = constexpr_math::fast_cos<precision, float>{};
-    //constexpr auto c3 = constexpr_math::fast_cos<precision, int>{};
-    std::cout << c.cos(60.1) << std::endl;
+int main(int argc, char *argv[]) {
+
+    using namespace std;
+    using namespace std::chrono_literals;
+
+    vector<double> daily_price = { 100.3, 101.5, 99.2, 105.1, 101.93,
+                                   96.7, 97.6, 103.9, 105.8, 101.2};
+
+    double average = 0.0;
+    std::atomic_flag still_busy = true; // atomic<bool>
+
+    // Calculate average in a separate thread
+    std::thread thread {
+        [&daily_price, &average, &still_busy](){
+            for (auto p: daily_price){
+                std::this_thread::sleep_for(2ms);
+                average += p;
+            }
+            average /= daily_price.size();
+            still_busy.clear(std::memory_order_release);
+
+            std::this_thread::sleep_for(2s);
+        }
+    };
+
+    while (still_busy.test_and_set(std::memory_order_acquire)){
+        cout << "..."; // "progress bar"
+        std::this_thread::sleep_for(1ms);
+    }
+    cout << "Average value: " << average << endl;
+
+    thread.join(); // wait until the thread finishes
+    cout << "Worker has finished working" << endl;
 }
