e1d110c6a3b8b482a30f9de683908584ad6ee168
Georgy Pashkov <gosha.pashkov@gmail.com>
2020-04-11 20:01:52 +0200

    Add async with different policies

--- main.cpp
+++ main.cpp
@@ -13,32 +13,26 @@ int main(int argc, char *argv[]) {
     vector<double> daily_price = { 100.3, 101.5, 99.2, 105.1, 101.93,
                                    96.7, 97.6, 103.9, 105.8, 101.2};
 
-    std::promise<double> average_promise;
-
-    // Calculate average in a separate thread
-    std::thread thread {
-        [&daily_price, &average_promise](){
+    // Async will take care of thread creation
+    auto future_average = std::async(std::launch::deferred,
+        [&daily_price](){
+            std::cout << "Calculation started..." << std::endl;
             auto average = 0.0;
-
             for (auto p: daily_price){
                 std::this_thread::sleep_for(200ms);
                 average += p;
             }
             average /= daily_price.size();
-            average_promise.set_value(average);
-
-            std::this_thread::sleep_for(2s);
+            return average;
         }
-    };
+    );
+    std::this_thread::sleep_for(200ms);
 
-    auto future_average = average_promise.get_future();
+    // Wait is optional
+    //while (future_average.wait_for(100ms) != future_status::ready){
+    //    cout << "."; // "progress bar"
+    //    cout.flush();
+    //}
+    cout << "Average value: " << flush << future_average.get() << endl;
 
-    while (future_average.wait_for(100ms) != future_status::ready){
-        cout << "."; // "progress bar"
-        cout.flush();
-    }
-    cout << "Average value: " << future_average.get() << endl;
-
-    thread.join(); // wait until the thread finishes
-    cout << "Worker has finished working" << endl;
 }
